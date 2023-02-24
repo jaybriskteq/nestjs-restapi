7fe40faee716b2b25a137b462cb35483d41d1918
jaybriskteq <jay.ranpariya@briskteq.com>
2023-02-24 11:31:24 +0530

    REST api creating

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'shipwreck roast',
+      brand: 'Buddy brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    console.log(typeof id);
+    const coffee = this.coffees.find((i) => i.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`coffee ${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    // if (existingCoffee) {
+    // }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((i) => i.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
