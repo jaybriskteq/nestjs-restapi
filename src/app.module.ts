e219f377f5c597805870ae78822b23c5d3a7f6bb
jaybriskteq <jay.ranpariya@briskteq.com>
2023-03-01 15:33:49 +0530

    add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Mongoose } from 'mongoose';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://JayRanpariya:jay04052002@node.tnxl6.mongodb.net/nestjs-DB?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
